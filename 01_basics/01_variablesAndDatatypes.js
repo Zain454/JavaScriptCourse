6783ea4a606da02ed50a0787800271e8943e3ff8
zain <72177552+Zain454@users.noreply.github.com>
2023-09-26 15:46:36 +0000

    learnt about variables

---
+++ 01_basics/01_variablesAndDatatypes.js
@@ -0,0 +1,8 @@
+console.log("This github contains code for the ultimate JavaScript course video no.2 ");
+
+// var a = 67
+
+let a = 67  // a contains 67
+console.log(a);
+a = "harry"
+console.log(a);
